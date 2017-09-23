908933af100bfaf9922159237b5b3ef7b93714da
Shovon <shovon284@gmail.com>
2017-09-23 22:56:47 +0600

    added index.html as home

--- server.js
+++ server.js
@@ -1,21 +1,46 @@
 var Hapi = require('hapi');
 
-var server = Hapi.Server();
+var server = new Hapi.Server();
 
 server.connection({
     host: 'localhost',
     port:3000
 });
 
+
+
+
+//dynamic route
 server.route({
     method: 'GET',
-    path:'/',
-    handler: function(request,replay){
-        replay('Hi!!');
+    path: '/{locations*2}',
+    handler: function (request, reply) {
+        const location = request.params.locations.split('/');
+        reply('So you wanna go ' + encodeURIComponent(location[1]) + ' from ' + encodeURIComponent(location[0]) + '!');
     }
-    
 });
 
-server.start(function(){
+//static route
+server.register(require('inert'), (err) => {
+
+    if (err) {
+        throw err;
+    }
+
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: function (request, reply) {
+            reply.file('./user/home.html');
+        }
+    });
+
+});
+
+
+server.start(function(err){
+    if (err) {
+            throw err;
+        }
     console.log('server started');
 });
